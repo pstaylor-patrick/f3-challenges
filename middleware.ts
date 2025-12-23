fbc48fce4f3029d097be20235eceeb27a9cb59a6
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-12-23 11:42:53 -0600

    Remove middleware redirect to Google Forms

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware() {
-  return NextResponse.redirect("https://forms.gle/tKCPRXHazWBryjyo8");
-}
-
-export const config = {
-  matcher: "/:path*",
-};
